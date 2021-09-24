e085eb49f5c0d8535f0563b35b70ddc3c94ab0a6
bettyzhuangg <87810403+bettyzhuangg@users.noreply.github.com>
2021-09-24 16:08:13 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:bettyzhuangg<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:bettyzhuangg/SisCode.git main:gh-pages
+git push -f git@github.com:bettyzhuangg/Assignment-3.git main:gh-pages
 
 cd -
